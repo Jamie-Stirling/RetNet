9e9efa994c01ddfbc7c38ee3a5d34ab3e526017f
Jamie-Stirling <stirlingj00@gmail.com>
2023-07-24 11:09:38 +0100

    make default implementation real-valued (xpos) and fix gating (+ to *)

--- src/retention.py
+++ src/retention.py
@@ -3,39 +3,24 @@ import math
 import torch
 import torch.nn as nn
 
-from util import ComplexGroupNorm
+from xpos_relative_position import XPOS
 
 class SimpleRetention(nn.Module):
-    def __init__(self, hidden_size, gamma, precision="single"):
+    def __init__(self, hidden_size, gamma):
         """
         Simple retention mechanism based on the paper
         "Retentive Network: A Successor to Transformer for Large Language Models"[https://arxiv.org/pdf/2307.08621.pdf]
         """
         super(SimpleRetention, self).__init__()
 
-        if precision == "half":
-            raise NotImplementedError("batchmm does not support half precision complex yet.")
-            self.complex_type = torch.complex32
-            self.real_type = torch.float16
-        elif precision == "single":
-            self.complex_type = torch.complex64
-            self.real_type = torch.float32
-
-        self.precision = precision
         self.hidden_size = hidden_size
         self.gamma = gamma
 
-        self.i = torch.complex(torch.tensor(0.0), torch.tensor(1.0))
-
-        self.W_Q = nn.Parameter(torch.randn(hidden_size, hidden_size, dtype=self.real_type) / hidden_size)
-        self.W_K = nn.Parameter(torch.randn(hidden_size, hidden_size, dtype=self.real_type) / hidden_size)
-        self.W_V = nn.Parameter(torch.randn(hidden_size, hidden_size, dtype=self.real_type) / hidden_size)
+        self.W_Q = nn.Parameter(torch.randn(hidden_size, hidden_size) / hidden_size)
+        self.W_K = nn.Parameter(torch.randn(hidden_size, hidden_size) / hidden_size)
+        self.W_V = nn.Parameter(torch.randn(hidden_size, hidden_size) / hidden_size)
         
-
-        self.theta = torch.randn(hidden_size) / hidden_size
-        self.theta = nn.Parameter(self.theta)
-
-        
+        self.xpos = XPOS(hidden_size)
 
     def forward(self, X):
         """
@@ -45,24 +30,13 @@ class SimpleRetention(nn.Module):
         sequence_length = X.shape[1]
         D = self._get_D(sequence_length)
 
-        if X.dtype != self.complex_type:
-            X = torch.complex(X, torch.zeros_like(X)).to(self.complex_type)
-        
-        i = self.i.to(X.device)
-        ns = torch.arange(1, sequence_length + 1, dtype=self.real_type, device=X.device)
-        ns = torch.complex(ns, torch.zeros_like(ns)).to(self.complex_type)
-        Theta = []
+        Q = (X @ self.W_Q)
+        K = (X @ self.W_K)
 
-        for n in ns:
-            Theta.append(torch.exp(i * n * self.theta))
-        
-        Theta = torch.stack(Theta, dim=0)
-        
-        Theta_bar = Theta.conj()
+        Q = self.xpos(Q)
+        K = self.xpos(K, downscale=True)
 
-        Q = (X @ self.W_Q.to(self.complex_type)) * Theta.unsqueeze(0)
-        K = (X @ self.W_K.to(self.complex_type)) * Theta_bar.unsqueeze(0)
-        V = X @ self.W_V.to(self.complex_type)
+        V = X @ self.W_V
         att = (Q @ K.permute(0, 2, 1)) * D.unsqueeze(0)
         
         return att @ V
@@ -70,41 +44,38 @@ class SimpleRetention(nn.Module):
     def forward_recurrent(self, x_n, s_n_1, n):
         """
         Recurrent representation of the retention mechanism.
-        x_n: (batch_size, hidden_size)
-        s_n_1: (batch_size, hidden_size)
+        x_n: (batch_size, 1, hidden_size)
+        s_n_1: (batch_size, hidden_size, hidden_size)
         """
-        if x_n.dtype != self.complex_type:
-            x_n = torch.complex(x_n, torch.zeros_like(x_n)).to(self.complex_type)
-        
-        n = torch.tensor(n, dtype=self.complex_type, device=x_n.device)
 
-        Theta = torch.exp(self.i * n * self.theta)
-        Theta_bar = Theta.conj()
+        Q = (x_n @ self.W_Q)
+        K = (x_n @ self.W_K)
 
-        Q = (x_n @ self.W_Q.to(self.complex_type)) * Theta
-        K = (x_n @ self.W_K.to(self.complex_type)) * Theta_bar
-        V = x_n @ self.W_V.to(self.complex_type)
+        Q = self.xpos(Q, n+1)
+        K = self.xpos(K, n+1, downscale=True)
 
-        # K: (batch_size, hidden_size)
-        # V: (batch_size, hidden_size)
+        V = x_n @ self.W_V
+
+        # K: (batch_size, 1, hidden_size)
+        # V: (batch_size, 1, hidden_size)
         # s_n_1: (batch_size, hidden_size, hidden_size)
         # s_n = gamma * s_n_1 + K^T @ V
 
-        s_n = self.gamma * s_n_1 + K.unsqueeze(2) @ V.unsqueeze(1)
+        s_n = self.gamma * s_n_1 + (K.transpose(-1, -2) @ V)
         
-        return (Q.unsqueeze(1) @ s_n).squeeze(1), s_n
+        return (Q @ s_n), s_n
     
     def _get_D(self, sequence_length):
         # D[n,m] = gamma ** (n - m) if n >= m else 0
-        D = torch.zeros((sequence_length, sequence_length), dtype=self.real_type, requires_grad=False)
+        D = torch.zeros((sequence_length, sequence_length), requires_grad=False)
         for n in range(sequence_length):
             for m in range(sequence_length):
                 if n >= m:
                     D[n, m] = self.gamma ** (n - m)
-        return D.to(self.complex_type)
+        return D
 
 class MultiScaleRetention(nn.Module):
-    def __init__(self, hidden_size, heads, precision="single"):
+    def __init__(self, hidden_size, heads):
         """
         Multi-scale retention mechanism based on the paper
         "Retentive Network: A Successor to Transformer for Large Language Models"[https://arxiv.org/pdf/2307.08621.pdf]
@@ -112,24 +83,15 @@ class MultiScaleRetention(nn.Module):
         super(MultiScaleRetention, self).__init__()
         self.hidden_size = hidden_size
         self.heads = heads
-        self.precision = precision
         assert hidden_size % heads == 0, "hidden_size must be divisible by heads"
         self.head_size = hidden_size // heads
-
-        if precision == "half":
-            raise NotImplementedError("batchmm does not support half precision complex yet.")
-            self.complex_type = torch.complex32
-            self.real_type = torch.float16
-        elif precision == "single":
-            self.complex_type = torch.complex64
-            self.real_type = torch.float32
         
-        self.gammas = (1 - torch.exp(torch.linspace(math.log(1/32), math.log(1/512), heads, dtype=self.real_type))).detach().cpu().tolist()
+        self.gammas = (1 - torch.exp(torch.linspace(math.log(1/32), math.log(1/512), heads))).detach().cpu().tolist()
 
         self.swish = lambda x: x * torch.sigmoid(x)
-        self.W_G = nn.Parameter(torch.randn(hidden_size, hidden_size, dtype=self.complex_type) / hidden_size)
-        self.W_O = nn.Parameter(torch.randn(hidden_size, hidden_size, dtype=self.complex_type) / hidden_size)
-        self.group_norm = ComplexGroupNorm(heads, hidden_size)
+        self.W_G = nn.Parameter(torch.randn(hidden_size, hidden_size) / hidden_size)
+        self.W_O = nn.Parameter(torch.randn(hidden_size, hidden_size) / hidden_size)
+        self.group_norm = nn.GroupNorm(heads, hidden_size)
 
         self.retentions = nn.ModuleList([
             SimpleRetention(self.head_size, gamma) for gamma in self.gammas
@@ -139,9 +101,7 @@ class MultiScaleRetention(nn.Module):
         """
         parallel representation of the multi-scale retention mechanism
         """
-        if X.dtype != self.complex_type:
-            X = torch.complex(X, torch.zeros_like(X)).to(self.complex_type)
-        
+
         # apply each individual retention mechanism to a slice of X
         Y = []
         for i in range(self.heads):
@@ -150,26 +110,27 @@ class MultiScaleRetention(nn.Module):
         Y = torch.cat(Y, dim=2)
         Y = self.group_norm(Y.reshape(-1, self.hidden_size)).reshape(X.shape)
 
-        return (self.swish(X @ self.W_G.to(self.complex_type)) + Y) @ self.W_O.to(self.complex_type)
+        return (self.swish(X @ self.W_G) * Y) @ self.W_O
     
     def forward_recurrent(self, x_n, s_n_1s, n):
         """
         recurrent representation of the multi-scale retention mechanism
-        """
-        if x_n.dtype != self.complex_type:
-            x_n = torch.complex(x_n, torch.zeros_like(x_n)).to(self.complex_type)
-        n = torch.tensor(n, dtype=self.complex_type, device=x_n.device)
+        x_n: (batch_size, 1, hidden_size)
+        s_n_1s: (batch_size, heads, head_size, head_size)
 
+        """
+    
         # apply each individual retention mechanism to a slice of X
         Y = []
         s_ns = []
         for i in range(self.heads):
             y, s_n = self.retentions[i].forward_recurrent(
-                x_n[:, i*self.head_size:(i+1)*self.head_size], s_n_1s[i], n
+                x_n[:, :, i*self.head_size:(i+1)*self.head_size], s_n_1s[i], n
                 )
             Y.append(y)
             s_ns.append(s_n)
         
         Y = torch.cat(Y, dim=1)
-        Y = self.group_norm(Y)
-        return (self.swish(x_n @ self.W_G.to(self.complex_type)) + Y) @ self.W_O.to(self.complex_type), s_ns
+        Y = self.group_norm(Y.reshape(-1, self.hidden_size)).reshape(x_n.shape)
+        
+        return (self.swish(x_n @ self.W_G) * Y) @ self.W_O, s_ns
--- src/retnet.py
+++ src/retnet.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 from retention import MultiScaleRetention
-from util import ComplexFFN, ComplexGroupNorm, ComplexLayerNorm
 
 class RetNet(nn.Module):
     def __init__(self, layers, hidden_dim, ffn_size, heads):
@@ -17,18 +16,30 @@ class RetNet(nn.Module):
             for _ in range(layers)
         ])
         self.ffns = nn.ModuleList([
-            ComplexFFN(hidden_dim, ffn_size)
+            nn.Sequential(
+                nn.Linear(hidden_dim, ffn_size),
+                nn.SiLU(),
+                nn.Linear(ffn_size, hidden_dim)
+            )
             for _ in range(layers)
         ])
-        self.layer_norm = ComplexLayerNorm(hidden_dim)
+        self.layer_norms_1 = nn.ModuleList([
+            nn.LayerNorm(hidden_dim)
+            for _ in range(layers)
+        ])
+        self.layer_norms_2 = nn.ModuleList([
+            nn.LayerNorm(hidden_dim)
+            for _ in range(layers)
+        ])
     
     def forward(self, X):
         """
         X: (batch_size, sequence_length, hidden_size)
         """
         for i in range(self.layers):
-            Y = self.retentions[i](self.layer_norm(X)) + X
-            X = self.ffns[i](self.layer_norm(Y)) + Y
+            Y = self.retentions[i](self.layer_norms_1[i](X)) + X
+           
+            X = self.ffns[i](self.layer_norms_2[i](Y)) + Y
 
         return X
 
@@ -40,79 +51,10 @@ class RetNet(nn.Module):
         """
         s_ns = []
         for i in range(self.layers):
-            o_n, s_n = self.retentions[i].forward_recurrent(self.layer_norm(x_n), s_n_1s[i], n)
+            # list index out of range
+            o_n, s_n = self.retentions[i].forward_recurrent(self.layer_norms_1[i](x_n), s_n_1s[i], n)
             y_n = o_n + x_n
             s_ns.append(s_n)
-            x_n = self.ffns[i](self.layer_norm(y_n)) + y_n
+            x_n = self.ffns[i](self.layer_norms_2[i](y_n)) + y_n
         
         return x_n, s_ns
-
-class RetNetCLM(nn.Module):
-    def __init__(self, layers, hidden_dim, ffn_size, heads, vocab_size):
-        """
-        NOTE: softmax not included!
-        """
-        super(RetNetCLM, self).__init__()
-        self.layers = layers
-        self.hidden_dim = hidden_dim
-        self.ffn_size = ffn_size
-        self.heads = heads
-        self.vocab_size = vocab_size
-
-        self.retnet = RetNet(layers, hidden_dim, ffn_size, heads)
-        self.embed = nn.Embedding(vocab_size, hidden_dim)
-        self.proj = nn.Parameter(torch.randn(hidden_dim, vocab_size, dtype=torch.float32) / hidden_dim)
-    
-    def forward(self, input_ids):
-        """
-        input_ids: (batch_size, sequence_length)
-        """
-        X = self.embed(input_ids)
-        X = self.retnet(X)
-        X = X @ self.proj.to(X.dtype)
-
-        return X.real
-    
-    def forward_recurrent(self, input_ids, s_n_1s, n):
-        """
-        input_ids: (batch_size)
-        s_n_1s: list of lists of tensors of shape (batch_size, hidden_size // heads, hidden_size // heads)
-        """
-        X = self.embed(input_ids)
-        X, s_ns = self.retnet.forward_recurrent(X, s_n_1s, n)
-        X = X @ self.proj.to(X.dtype)
-
-        return X.real, s_ns
-    
-    def sample(self, input_ids, sample_length, temperature=1.0):
-        """
-        input_ids: (batch_size, sequence_length)
-        s_n_1s: list of lists of tensors of shape (batch_size, hidden_size // heads, hidden_size // heads)
-        """
-        s_n_1s = [
-            [
-                torch.zeros(self.hidden_dim // self.heads, self.hidden_dim // self.heads, dtype=torch.complex64).unsqueeze(0).repeat(input_ids.shape[0], 1, 1)
-                for _ in range(self.heads)
-            ] for _ in range(self.layers)
-        ]
-        for i in range(input_ids.shape[1]):
-            X, s_n_1s = self.forward_recurrent(input_ids[:, i], s_n_1s, i+1)
-        
-        # get softmax of x (real part only)
-        X = X.real / temperature
-        X = torch.softmax(X, dim=-1)
-        X = torch.multinomial(X, num_samples=1)
-        next_char = X[:, -1]
-        output_ids = []
-        # now start sampling!
-        for i in range(sample_length):
-            X, s_n_1s = self.forward_recurrent(next_char, s_n_1s, i+1)
-            X = X.real / temperature
-            X = torch.softmax(X, dim=-1)
-            X = torch.multinomial(X, num_samples=1)
-            next_char = X[:, -1]
-            output_ids.append(next_char)
-
-        output_ids = torch.stack(output_ids, dim=1)
-
-        return output_ids
